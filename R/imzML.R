5fcf0b53a93296f9c5e7d6899e182a2edb6938d2
Kylie Bemis <k.bemis@northeastern.edu>
2023-08-05 15:38:22 -0400

    pass R CMD check

--- R/imzML.R
+++ R/imzML.R
@@ -5,7 +5,8 @@
 setClass("ImzML", contains = "SimpleList")
 
 # note: Does *_NOT_* validate the xml mapping
-.valid.ImzML <- function(object) {
+.valid_ImzML <- function(object)
+{
 	errors <- NULL
 	required_mzML_elements <- c("fileDescription", "softwareList",
 		"instrumentConfigurationList", "dataProcessingList", "run")
@@ -20,13 +21,25 @@ setClass("ImzML", contains = "SimpleList")
 	if ( is.null(errors) ) TRUE else errors
 }
 
-setValidity("ImzML", .valid.ImzML)
+setValidity("ImzML", .valid_ImzML)
 
+# note: don't export constructor (not to be instantiated directly)
+.new_ImzML <- function(..., validate = TRUE)
+{
+	object <- SimpleList(...)
+	object <- as(object, "ImzML")
+	if ( validate )
+		validObject(object)
+	object
+}
+
+# show utility
 .shorten_strings <- function(x, n = 12L)
 {
 	ifelse(nchar(x) > n, paste0(substr(x, 1, n), "..."), x)
 }
 
+# show utility
 .show_list_names <- function(x, n = 12L)
 {
 	for ( i in seq_along(x) ) {
@@ -40,6 +53,32 @@ setValidity("ImzML", .valid.ImzML)
 	}
 }
 
+setMethod("show", "ImzML", function(object) {
+	cat(class(object), ": ", metadata(object)$source, "\n\n", sep="")
+	.show_list_names(object, n=7L)
+})
+
+setAs("list", "ImzML", function(from) {
+	from <- SimpleList(from)
+	as(from, "ImzML")
+})
+
+# test if cvParam (does *_NOT_* check ontology validity)
+.is_cvParam <- function(x) {
+	is.character(x) && all(c("cv", "id", "name") %in% names(x))
+}
+
+# test if userParam
+.is_userParam <- function(x) {
+	is.character(x) && "name" %in% names(x)
+}
+
+# test if cvParam OR userParam
+.is_param <- function(x) {
+	.is_cvParam(x) || .is_userParam(x)
+}
+
+# print cvParams and userParams
 .show_params <- function(x, n = 12L, collapse = ", ")
 {
 	a <- attributes(x)
@@ -58,9 +97,9 @@ setValidity("ImzML", .valid.ImzML)
 			{
 				if ( is.list(tag) ) {
 					"..."
-				} else if ( "id" %in% names(tag) ) {
+				} else if ( .is_cvParam(tag) ) {
 					tag["name"] 	# cvParam
-				} else if ( "name" %in% names(tag) ) {
+				} else if ( .is_userParam(tag) ) {
 					"[userParam]"	# userParam
 				} else {
 					tag[1L]			# other tags (e.g. "softwareRef")
@@ -102,25 +141,6 @@ print.imzplist <- function(x, n = 12L, collapse = ", ", ...)
 	invisible(x)
 }
 
-setMethod("show", "ImzML", function(object) {
-	cat(class(object), ": ", metadata(object)$source, "\n\n", sep="")
-	.show_list_names(object, n=7L)
-})
-
-setAs("list", "ImzML", function(from) {
-	from <- SimpleList(from)
-	as(from, "ImzML")
-})
-
-# no exported constructor (not user-instantiated)
-new_ImzML <- function(..., validate = TRUE) {
-	object <- SimpleList(...)
-	object <- as(object, "ImzML")
-	if ( validate )
-		validObject(object)
-	object
-}
-
 #### parse an imzML file ####
 ## --------------------------
 
@@ -128,7 +148,7 @@ parseImzML <- function(file, ...)
 {
 	path <- normalizePath(file, mustWork=TRUE)
 	parse <- .Call(C_parseImzML, path)
-	parse <- new_ImzML(parse, validate=FALSE)
+	parse <- .new_ImzML(parse, validate=FALSE)
 	metadata(parse)$source <- file
 	metadata(parse)$location <- dirname(file)
 	metadata(parse)$name <- basename(file)
